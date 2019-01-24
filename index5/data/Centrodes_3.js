a8b92944a20a2b81a4a5ba48f2b32c1d0423236d
lemarocenentreprises <46978024+lemarocenentreprises@users.noreply.github.com>
2019-01-24 07:24:11 +0100

    upload website

---
+++ index5/data/Centrodes_3.js
@@ -0,0 +1,10 @@
+var json_Centrodes_3 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Province d'Errachidia Ø¥Ù‚Ù„ÙŠÙ… Ø§Ù„Ø±Ø´ÙŠØ¯ÙŠØ©", "Descriptio": null, "NOMBRE": 0.0, "TRIBUNAL": "Errachidia", "nombre_ent": "492", "NOMBRE_1": "0" }, "geometry": { "type": "Point", "coordinates": [ -4.339284478847931, 31.344782485128491 ] } },
+{ "type": "Feature", "properties": { "Name": "Province de Midelt Ø¥Ù‚Ù„ÙŠÙ… Ù…ÙŠØ¯Ù„Øª", "Descriptio": null, "NOMBRE": 1.0, "TRIBUNAL": "Midelt", "nombre_ent": "329", "NOMBRE_1": "1" }, "geometry": { "type": "Point", "coordinates": [ -4.817369593000076, 32.440776287271404 ] } },
+{ "type": "Feature", "properties": { "Name": "Province de Ouarzazate âµâµ‰âµ‡âµâµ‰âµŽ âµ âµ¡â´°âµ”âµ£â´°âµ£â´°âµœ Ø¥Ù‚Ù„ÙŠÙ… ÙˆØ±Ø²Ø²Ø§Øª", "Descriptio": null, "NOMBRE": 2.0, "TRIBUNAL": "Ouarzazate", "nombre_ent": "758", "NOMBRE_1": "2" }, "geometry": { "type": "Point", "coordinates": [ -7.069328555589723, 30.913799970797541 ] } },
+{ "type": "Feature", "properties": { "Name": "Province de Zagora âµâµ‰âµ‡âµâµ‰âµŽ âµ âµ£â´°â´³âµ“âµ”â´° Ø¥Ù‚Ù„ÙŠÙ… Ø²Ø§ÙƒÙˆØ±Ø©", "Descriptio": null, "NOMBRE": 4.0, "TRIBUNAL": "Zagora", "nombre_ent": "258", "NOMBRE_1": "4" }, "geometry": { "type": "Point", "coordinates": [ -5.927785879340317, 30.338915900768505 ] } }
+]
+}
---
+++ index6/data/Centrodes_3.js
@@ -0,0 +1,10 @@
+var json_Centrodes_3 = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Province d'Errachidia Ø¥Ù‚Ù„ÙŠÙ… Ø§Ù„Ø±Ø´ÙŠØ¯ÙŠØ©", "Descriptio": null, "NOMBRE": 0.0, "TRIBUNAL": "Errachidia", "nombre_ent": "492", "NOMBRE_1": "0", "TRIBUNAL_1": "Errachidia", "nombre_201": "462", "nombre_202": "359", "nombre_203": "464", "NOMBRE_12": "0" }, "geometry": { "type": "Point", "coordinates": [ -4.339284478847931, 31.344782485128491 ] } },
+{ "type": "Feature", "properties": { "Name": "Province de Midelt Ø¥Ù‚Ù„ÙŠÙ… Ù…ÙŠØ¯Ù„Øª", "Descriptio": null, "NOMBRE": 1.0, "TRIBUNAL": "Midelt", "nombre_ent": "329", "NOMBRE_1": "1", "TRIBUNAL_1": "Midelt", "nombre_201": "235", "nombre_202": "279", "nombre_203": "338", "NOMBRE_12": "1" }, "geometry": { "type": "Point", "coordinates": [ -4.817369593000076, 32.440776287271404 ] } },
+{ "type": "Feature", "properties": { "Name": "Province de Ouarzazate âµâµ‰âµ‡âµâµ‰âµŽ âµ âµ¡â´°âµ”âµ£â´°âµ£â´°âµœ Ø¥Ù‚Ù„ÙŠÙ… ÙˆØ±Ø²Ø²Ø§Øª", "Descriptio": null, "NOMBRE": 2.0, "TRIBUNAL": "Ouarzazate", "nombre_ent": "758", "NOMBRE_1": "2", "TRIBUNAL_1": "Ouarzazate", "nombre_201": "669", "nombre_202": "669", "nombre_203": "712", "NOMBRE_12": "2" }, "geometry": { "type": "Point", "coordinates": [ -7.069328555589723, 30.913799970797541 ] } },
+{ "type": "Feature", "properties": { "Name": "Province de Zagora âµâµ‰âµ‡âµâµ‰âµŽ âµ âµ£â´°â´³âµ“âµ”â´° Ø¥Ù‚Ù„ÙŠÙ… Ø²Ø§ÙƒÙˆØ±Ø©", "Descriptio": null, "NOMBRE": 4.0, "TRIBUNAL": "Zagora", "nombre_ent": "258", "NOMBRE_1": "4", "TRIBUNAL_1": "Zagora", "nombre_201": "149", "nombre_202": "192", "nombre_203": "204", "NOMBRE_12": "4" }, "geometry": { "type": "Point", "coordinates": [ -5.927785879340317, 30.338915900768505 ] } }
+]
+}
